4f7a148024af1e1f560ecf5fed23cf4afc44833c
Aman Gupta <amangupta0105@gmail.com>
2025-07-26 17:30:36 +0530

    setup - router tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import DashBoard from "./screens/DashBoard";
+
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(<Route path="/" element={<DashBoard />} />)
+  );
 
   return (
     <>
-
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
